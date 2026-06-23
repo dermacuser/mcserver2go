3c36e9d7590d6d3bee7376c0ee08bf6b358474f1
Justin <122570956+dermacuser@users.noreply.github.com>
2026-06-23 20:42:19 +0200

    Update Java version and fix script syntax errors

--- server2go.sh
+++ server2go.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#! /bin/bash
 sudo apt update -y && sudo apt upgrade -y
 #TEST FOR DEBUG
 sudo apt remove openjdk-21-jdk openjdk-21-jre -y
 
-sudo apt install openjdk-21-jdk openjdk-21-jre -y
+sudo apt install openjdk-25-jdk openjdk-25-jre -y
 mkdir minecraftserver
 cd minecraftserver
-curl https://fill-data.papermc.io/v1/objects/0555a0b0468a5198d8fb1a16e1f9e95c81a917a2dc8f2e09867b4044742f6401/paper-26.1.2-72.jar --output paper.jar
+curl https://fill-data.papermc.io/v1/objects/0555a0b0468a5198d8fb1a16e1f9e95c81a917a2dc8f2e09867b4044742f6401/paper-26.1.2-72. jar -- output paper. jar
 echo "#!/bin/bash
 
 #Standard Minecraft
 cd /home/minecraft/server/
-exec java -Xmx2048M -Xms512M -jar paper.jar nogui" > start.sh
+exec java -Xmx2048M -Xms512M -jar paper. jar nogui" > start.sh
 
 #DEBUG ONLY !!!
 echo "DISABLE THIS OPTION BEFORE RELEASE !!! "
@@ -19,6 +19,6 @@ echo "eula=true" > eula.txt
 #DEBUG ONLY !!!
 
 #echo "Server Finished now change eula. txt and server.properties"
-echo "Server Starting ... "
+echo "Server Starting ...
 wait 2
-bash start.sh
+bash start. sh

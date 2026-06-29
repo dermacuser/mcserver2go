516515166ae0cdd62e09cf84a480b262c9b1f189
Justin <122570956+dermacuser@users.noreply.github.com>
2026-06-29 22:21:58 +0200

    Modified for Waterfall setups

--- server2go.sh
+++ server2go.sh
@@ -5,20 +5,19 @@ sudo apt update -y && sudo apt upgrade -y
 #sudo apt remove openjdk-21-jdk openjdk-21-jre -y
 
 sudo apt install openjdk-25-jdk openjdk-25-jre -y
-mkdir minecraftserver
-cd minecraftserver
+mkdir servername
+cd servername
 curl https://fill-data.papermc.io/v1/objects/0555a0b0468a5198d8fb1a16e1f9e95c81a917a2dc8f2e09867b4044742f6401/paper-26.1.2-72.jar --output paper.jar
 echo "#!/bin/bash
 
-#Standard Minecraft
-cd /home/minecraft/server/
-exec java -Xmx2048M -Xms512M -jar paper.jar nogui" > start.sh
+cd /home/justin/servername || exit 1
+screen -DmS servername java -Xmx2G -Xms512M -jar paper.jar nogui" > start.sh
 
-#DEBUG ONLY !!!
-#echo "DISABLE THIS OPTION BEFORE RELEASE !!! "
-#echo "eula=true" > eula.txt
-#DEBUG ONLY !!!
+DEBUG ONLY !!!
+echo "DISABLE THIS OPTION BEFORE RELEASE !!! "
+echo "eula=true" > eula.txt
+DEBUG ONLY !!!
 
 echo "Server Finished now change eula. txt and server.properties"
-echo "Server Starting ..."
-bash start.sh
+echo "Server Starting ... (DISABLED FOR WATERFALL PURPOSE, PLEASE ADJUST THE SETTINGS!!!)"
+#bash start.sh
